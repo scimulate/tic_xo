aa0f81b51e1b972c7155cb83c5ab96a5dae0c995
Samuel Jacobs <samjacobs@scimulate.com>
2025-11-08 10:05:45 -0500

    Added a very basic board printing function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+fn main()
+{
+    let mut board = [0i8; 9];
+    //play(&board);
+    board[0] = 4;
+    print_board(& board);
+}
+
+fn print_board(board: &[i8])
+{
+    println!("Tic_XO Current Board:");
+    println!("{}{}{}\n{}{}{}\n{}{}{}", marker(board[0]),
+                                       marker(board[1]),
+                                       marker(board[2]),
+                                       marker(board[3]),
+                                       marker(board[4]),
+                                       marker(board[5]),
+                                       marker(board[6]),
+                                       marker(board[7]),
+                                       marker(board[8]));
+}
+
+fn play()
+{
+    //printBoard();
+}
+
+fn play_headless()
+{
+
+}
+
+fn marker(val:i8) -> char
+{
+    if val < 0
+    {
+        'O'
+    }
+    else if val == 0
+    {
+        ' '
+    }
+    else
+    {
+        'X'
+    }
 }
