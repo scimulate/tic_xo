6b820b8649ff88696c7955a74e6da247d0f8f08f
scimulate <samjacobs@scimulate.com>
2025-11-15 09:36:31 -0500

    Updating board.rs, moving code from main.rs

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,25 @@
 use crate::marker;
 
+pub fn check_board(board: &[i8]) -> bool
+{
+    // The board data is designed to make it trivial to check for a winner. If
+    // any row, column, or diagonal adds to |3|, there is a winner. Otherwise,
+    // no winner yet. If the board is full and the game is a draw, have that 
+
+    let mut game_won = false;
+
+    // Check horizontals
+    //let mut ct = 0;
+    for ct in 1..=10
+    {
+        println!("{}", ct);
+    }
+    // Check verticals
+    // Check diagonals
+
+    game_won
+}
+
 pub fn print_board(board: &[i8])
 {
     println!("Tic_XO Current Board:");
--- src/main.rs
+++ src/main.rs
@@ -11,21 +11,8 @@ fn main()
     //check_board(&board);
 }
 
-fn check_board(board: &[i8])// -> bool
-{
-    // The board data is designed to make it trivial to check for a winner. If
-    // any row, column, or diagonal adds to |3|, there is a winner. Otherwise,
-    // no winner yet. If the board is full and the game is a draw, have that 
+/*
 
-    // Check horizontals
-    //let mut ct = 0;
-    for ct in 1..=10
-    {
-        println!("{}", ct);
-    }
-    // Check verticals
-    // Check diagonals
-}
 
 fn play()
 {
@@ -36,4 +23,4 @@ fn play_headless()
 {
 
 }
-
+*/
