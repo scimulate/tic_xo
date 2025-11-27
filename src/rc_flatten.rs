730188ee2f8154b8522fd9aa436c5418ce9db7d3
scimulate <samjacobs@scimulate.com>
2025-11-27 10:29:12 -0500

    Initial commit.

---
+++ src/rc_flatten.rs
@@ -0,0 +1,9 @@
+pub fn rc_flatten(row: i8, col: i8) -> i8
+{
+    /* 
+    This function acts as a translation between the 2D board position and an
+    index of the board array.
+    */
+
+    3*row+col
+}
