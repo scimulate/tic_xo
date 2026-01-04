334ad0baa10e4e9d8e6a29734d4eab1d75cb10ba
scimulate <samjacobs@scimulate.com>
2026-01-03 20:22:15 -0500

    check_board() working minimally well.

--- src/board.rs
+++ src/board.rs
@@ -5,26 +5,68 @@ pub fn check_board(board: &[i8]) -> bool
 {
     // The board data is designed to make it trivial to check for a winner. If
     // any row, column, or diagonal adds to |3|, there is a winner. Otherwise,
-    // no winner yet. If the board is full and the game is a draw, have that 
+    // no winner yet.
 
     let mut game_won = false;
     let mut total = 0;
 
     // Check horizontals
-    for ct in 1..=3
+    for row in 0..3
     {
-        total += board[rc_flatten(0, (ct-1).try_into().unwrap())];
+        total = 0;
+        for col in 0..3
+        {
+            total += board[rc_flatten(row, col)];
+            if total.abs() == 3
+            {
+                println!("Game won, row {}, player {}", row, marker::marker(total/3));
+                game_won = true;
+            }
+        }
+    }
+
+    // Check verticals
+    for col in 0..3
+    {
+        total = 0;
+        for row in 0..3
+        {
+            total += board[rc_flatten(row, col)];
+            if total.abs() == 3
+            {
+                println!("Game won, col {}, player {}", col, marker::marker(total/3));
+                game_won = true;
+            }
+        }
+    }
+
+    // Check diagonals (negative slope)
+    total = 0;
+    for ct in 0..3
+    {
+        total += board[rc_flatten(ct, ct)];
         if total.abs() == 3
         {
+            println!("Game won, diag 1, player {}", marker::marker(total/3));
             game_won = true;
         }
     }
 
-    println!("{}", total);
-    // Check verticals
-    // Check diagonals
+    // Check diagonals (positive slope)
+    total = 0;
+    for ct in 0..3
+    {
+        total += board[rc_flatten(ct, 2-ct)];
+        if total.abs() == 3
+        {
+            println!("Game won, diag 1, player {}", marker::marker(total/3));
+            game_won = true;
+        }
+    }
 
-    true
+    
+
+    game_won
 }
 
 pub fn print_board(board: &[i8])
